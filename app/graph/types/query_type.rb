b8c6b5f8a8bd67d2208daa057b5ac1b8da2ff5f0
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-10-27 09:18:34 +0900

    Add field articles

--- app/graph/types/query_type.rb
+++ app/graph/types/query_type.rb
@@ -2,6 +2,12 @@ QueryType = GraphQL::ObjectType.define do
   name "Query"
   description "The query root for this schema"
 
+  field :articles, types[ArticleType] do
+    resolve -> (obj, args, context) {
+      Article.all.includes(:comments)
+    }
+  end
+
   field :article do
     type ArticleType
     argument :id, !types.ID
